45aedb6d3b5cdcc6cdbe1ea43033e2c6ef936358
u0861355 <u0861355@eae-d-013>
2017-08-29 20:36:06 -0600

    Add project files.

---
+++ ConsoleApplication1/HexToInteger.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class HexToInteger
+    {
+        public static int HexToInt(string hex)
+        {
+            List<int> list = new List<int>();//list to collect all the string characters as integers to do math with later
+
+            int testAnswer = 0;
+            //*****THIS FOLLOWING TEST IS DONE FOR TESTING PURPOSES TO ASSERT THAT THE ANSWER IS EQUAL TO THE BUILT IN .NET FUNCTIONALITY FOR HEXT TO INT CONVERSION'S ANSWER AT THE END OF THIS METHOD
+            try
+            {
+                testAnswer = int.Parse(hex, System.Globalization.NumberStyles.HexNumber);//this will throw an error if input includes letters not in the range ABCDEF
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("you attempted to enter a hexadecimal value with letters outside of the acceptable range ABCDEF", ex.Message, ex.Data);
+            }
+                
+            
+            //END TESTING
+
+            for (int i = 0; i < hex.Length; i++)
+            {
+
+                //can check here that letter inputs do go outsdie ABCDEF:
+                int num;//only using tryparse for testing
+                if (!int.TryParse(hex[i].ToString(), out num))//if the input char at this point in the string is a letter then...
+                {
+                    Debug.Assert(hex[i] - 'A' <= 5 && hex[i] - 'A' >= 0);//ensure that it is within the proper range
+                }
+
+                if (hex[i] - 'A' <= 5 && hex[i] - 'A' >= 0)//this does not rely on tryparse like the old if conditional below, instead it just checks that the letters are within the ascii range of ABCDEF, which also means you will get an error if you use a letter beyond outside of these
+                //int num;
+                //if (!int.TryParse(hex[i].ToString(), out num))//if you can't convert the char in the hexstring to an integer then it must be a letter, and the letters must be converted to integers
+                {
+                    //Console.WriteLine("old: "+hex[i]);
+                    int temp;
+                    temp = (char)hex[i] - 55;//add the proper value to convert these letters ABCDEF to the correct numbers 10,11,12,13,14,15
+                    list.Add(temp);
+                }
+                else
+                {
+                    list.Add((int)hex[i] - '0');
+                }
+            }
+
+            double sum = 0;
+            double temporary;
+            int exponent = 0;
+
+            for (int i = list.Count() - 1; i >= 0; i--)
+            {
+                temporary = (list[i] * Math.Pow(16, exponent++));
+                sum += temporary;
+            }
+
+            Debug.Assert(testAnswer == (int)sum);//test to ensure conversion worked correctly
+
+            return (int)sum;
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    //integer to hex is below and hex to integer is in another file in this project HexToInteger
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Console.WriteLine(IntegerToHex.IntToHex(241));
+
+            Console.WriteLine(HexToInteger.HexToInt("FFG"));//make sure to check input to not permit letters above F?
+
+            //Console.WriteLine(IntegerToHex.IntToHex(HexToInteger.HexToInt("A5")));
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public class IntegerToHex
+    {
+        public static string IntToHex(int value)
+        {
+            //*****THIS FOLLOWING LINE IS DONE FOR TESTING PURPOSES TO ASSERT THAT THE ANSWER IS EQUAL TO THE BUILT IN .NET FUNCTIONALITY FOR INT TO HEX CONVERSION'S ANSWER AT THE END OF THIS METHOD
+            string hexValue = value.ToString("X");//THIS IS NOT USED TO DETERMINE THE ANSWER!!!
+            //END TESTING LINES
+            
+
+            int quotient = 1;
+            int remainder;
+            List<int> list = new List<int>();
+            string answer = "";
+            string thisString;
+            char thisChar;
+            List<int> listReversed = new List<int>();
+
+            //populate the list with the correct integer values as per the necessary calculations
+            while (quotient != 0)
+            {
+                quotient = value / 16;
+                remainder = value % 16;
+                list.Add(remainder);
+                value = quotient;
+            }
+
+            //if a value in the list is greater than 9 then convert it to the apropriate number corresponding to its ascii letter
+            for (int i = 0; i< list.Count(); i++)
+            {
+                Debug.Assert(list[i] <= 16 && list[i] >= 0);
+                if(list[i] > 9)
+                {
+                    int numChar = (list[i] - 10 + 65);//of course I could put + 55 here but it is less confusing to convert it first to 10 = 0, 11 = 1, etc...and then add 65 ('A' = 65), so that 10 = 'A' etc... 
+                    list[i] = (char)numChar;//this (char) cast is actually unnecessary I think
+                    
+                }
+            }
+       
+            //reverse the list to be in proper hexadecimal order
+            for(int i = 0; i < list.Count(); i++)
+            {
+                listReversed.Add(list[list.Count() - 1 - i]); 
+            }
+            list = listReversed;
+
+            //concatenate the whole list into a string and then return that string, taking into account that your above 10 integers must be converted into char-type letters and then into strings to concatenate with the answer string
+            foreach (int n in list)
+            {
+                if (n < 10)
+                {
+                    thisString = n.ToString();
+                }
+                else
+                {
+                    thisChar = (char)n;//if above 16 then use the temporary variable thisChar to convert the integer to a corresponding char type letter
+                    thisString = thisChar.ToString();//and then make that char letter into a string letter 
+                }
+                answer += thisString;//and finall concatenate
+            }
+
+            //here the test hexValue conversion is used to assert that our answer is correct
+            Debug.Assert(answer == hexValue);
+
+            return answer;
+        }
+    }
+}
